16802a8de6765c2557b4898713e40a043eea6b6a
kfennimore <32980838+kfennimore@users.noreply.github.com>
2018-02-06 16:55:23 -0500

    Update MainApplication.java

--- src/main/java/com/zipcodewilmington/MainApplication.java
+++ src/main/java/com/zipcodewilmington/MainApplication.java
@@ -3,5 +3,7 @@ package com.zipcodewilmington;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is a change");
+
     }
 }
